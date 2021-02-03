5d304e180e040cd8b3e33398d08b9b06082e2a97
Wirtos_new <Wirtos.new@gmail.com>
2021-02-03 15:25:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,34 +3,38 @@
 #include <stdlib.h> /* malloc, EXIT_**/
 
 int main(void) {
-    map_t(size_t) map; /* define custom map with size_t value type*/
-    map_init(&map);
-    printf("MAP INIT\n");
+    unsigned i;
     char *keys[] = {"k0", "k1", "k2", "k3", "k4", "k5", "k6", "k7", "k8", "k9"};
+    map_t(char *, unsigned) map; /* define custom map */
+    map_init(&map, map_string_cmp, map_string_hash);
+    printf("MAP INIT\n");
 
-    for (size_t i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         if (!map_set(&map, keys[i], i + 1)) { /* Handle setter error*/
-            printf("Failed map_set alloc on key %zu\n", i);
+            printf("Failed map_set alloc on key %u\n", i);
             goto fail;
         }
+        {
+            unsigned *val = map_get(&map, keys[i]);
 
-        size_t *val = map_get(&map, keys[i]);
-
-        if (val == NULL) {
-            /* Handle getter error, NULL pointer returned if there's no such key (shouldn't be possible in this example)*/
-            printf("Failed map_get on key %zu\n", i);
-            goto fail;
+            if (val == NULL) {
+                /* Handle getter error, NULL pointer returned if there's no such key (shouldn't be possible in this example)*/
+                printf("Failed map_get on key %u\n", i);
+                goto fail;
+            }
+            printf("'%s' -> %u\n", keys[i], *val);
         }
-        printf("'%s' -> %zu\n", keys[i], *val);
     }
-    printf("\nIterate key by key:\n"); /* Notice the fact that keys aren't sorted*/
-    const char *iter_key;
+    {
+        char *iter_key;
+        map_iter_t get_iter = map_iter(&map);
+        printf("\nIterate key by key:\n"); /* Notice the fact that keys aren't sorted*/
 
-    map_iter_t get_iter = map_iter(&map);
-    while ((iter_key = map_next(&map, &get_iter))) {
-        size_t ret = *map_get(&map, iter_key);
-        printf("'%s' -> %zu\n", iter_key, ret);
+        while ((map_next(&map, &get_iter, &iter_key))) {
+            unsigned ret = *map_get(&map, iter_key);
+            printf("'%s' -> %u\n", iter_key, ret);
 
+        }
     }
 
     map_delete(&map); /* Free entire map*/
